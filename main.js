3ebec3003f277fd1fc19436a9e7270a06053c5a6
Eri-Heriawan <83988432+EriHeriawan@users.noreply.github.com>
2023-11-17 20:42:17 +0700

    add file
    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Malam Sayangku", "Aku Sayang Banget Sama Kamu","Terima kasih untuk hari ini sayangku", "I Love You more sayangku 😘😘😘😘😘😍❤️"],
+        typeSpeed: 70
+    });
+}
